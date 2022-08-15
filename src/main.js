ec5da67ba13a5dc6568d5c808544d8147f61a15f
Carlos Seguessa <carlosseguessa@gmail.com>
2022-08-15 11:34:22 -0300

    ajustes en el modal y agrege funcion para boton de abrir y cerrar el modal

--- src/main.js
+++ src/main.js
@@ -1,17 +1,44 @@
 const closeButton = document.querySelector('.close-button');
 const closeButtondk = document.querySelector('.close-button-dk');
+const closeArrow = document.querySelector('.arrow-close');
+const closeArrowdk = document.querySelector('.arrow-close-dk');
 const loginButton = document.querySelector('.login');
 const modal = document.getElementById('modal-container');
+const hamburguerButton = document.getElementById('hamburguer-button');
+const hamburguerMenu = document.getElementById('hamburguer-menu');
+
+hamburguerMenu.classList.add('hidden');    
+
+const closeOpenModal = (metodo,classes) => {
+    if (metodo === 'add') {
+        modal.classList.add(...classes);
+    }else if (metodo === 'remove') {
+        modal.classList.remove(...classes);
+    }
+}
+
 
 loginButton.addEventListener('click', () => {
-    modal.classList.remove('hidden');
+    closeOpenModal('remove',['hidden']);
 });
 
 closeButton.addEventListener('click', () => {
-    modal.classList.add('hidden');
+    closeOpenModal('add',['hidden']);
 });
 
 closeButtondk.addEventListener('click', () => {
-    modal.classList.add('hidden');
+    closeOpenModal('add',['hidden']);
 });
 
+closeArrow.addEventListener('click', () => {
+    closeOpenModal('add',['hidden']);
+});
+
+closeArrowdk.addEventListener('click', () => {
+    closeOpenModal('add',['hidden']);
+});
+
+hamburguerButton.addEventListener('click', () => {
+    
+    hamburguerMenu.classList.toggle('hidden');
+});
